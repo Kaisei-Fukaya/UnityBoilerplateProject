72bb3210a12a29380f110c7396c9bce58c4c9ea0
Kaisei-Fukaya <kjfukaya@gmail.com>
2020-10-24 23:59:21 +0100

    Added simple tiggers

---
+++ BoilerplateProject/Assets/CODE_BASE/Monobehaviours/SimpleTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace KaiseiBoilerplate
+{
+    [RequireComponent(typeof(Collider))]
+	public class SimpleTrigger : MonoBehaviour
+	{
+        [SerializeField] UnityEvent events;
+
+        //Works with either trigger or non trigger colliders
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            events.Invoke();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            events.Invoke();
+        }
+    }
+}
